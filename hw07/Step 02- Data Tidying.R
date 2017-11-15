1becadfb4b896390c986a99f255d4dc91a505ae9
Ginni Braich <ginni.braich@gmail.com>
2017-11-15 11:48:28 -0800

    add hw07

---
+++ hw07/Step 02- Data Tidying.R
@@ -0,0 +1,53 @@
+library(ggplot2)
+library(dplyr)
+library(stringr)
+
+#Load lotr_raw from Step 1
+lotr_data <- read.delim("lotr_raw.tsv")
+
+glimpse(lotr_data)
+
+# Make Chapter column into two columns (number and name)
+# Rename columns after binding together
+lotr_data_split <- data.frame(do.call(rbind, strsplit(as.vector(lotr_data$Chapter), split = ":")))
+names(lotr_data_split) <- c("Chapter_Number", "Chapter_Name")
+lotr_data_clean_split <- cbind(lotr_data$Film, lotr_data_split, lotr_data$Character, lotr_data$Race, lotr_data$Words)
+ldcs2<- plyr::rename(lotr_data_clean_split, c("lotr_data$Film"="Film", "lotr_data$Character"="Character", "lotr_data$Race"="Race", "lotr_data$Words"="Words"))
+ldcs2
+
+#reoder Film factor levels
+old_levels <- levels(ldcs2$Film)
+j_order <- sapply(c("Fellowship", "Towers", "Return"),
+				  function(x) grep(x, old_levels))
+new_levels <- old_levels[j_order]
+
+
+# apply new factor levels to Film
+# change names of races (eg. Ainur to wizard, Men to Man)
+# drop races that are less frequent
+# reorder Race based on words spoken
+# arrange data on Race, Film, Words
+lotr_data_clean <- ldcs2 %>% 
+	mutate("Film" = factor(as.character(Film), new_levels),
+		   "Race" = plyr::revalue(Race, c(Ainur="Wizard", Men="Man"))) %>% 
+	filter(!(Race %in% c("Gollum", "Orc", "Ent", "Dead", "Nazgul"))) %>% 
+	mutate(Race = reorder(Race, Words, sum)) %>% 
+	arrange(Race,Film, Words) %>% 
+	droplevels
+glimpse(lotr_data_clean)
+
+##Make a plot about which race does the most talking by film
+
+lotr_dat %>% 
+	ggplot(aes(Race, Words)) +geom_col() + facet_wrap(~Film)
+	
+plot1 <- lotr_data_clean %>% 
+	ggplot(aes(Race, Words)) + 
+	geom_col() +
+	facet_wrap(~Film) +
+	labs(y="Total Words Spoken") 
+ggsave("barchart_total_words_by_race_by_film.png", plot1)
+
+#write data to file
+write.table(lotr_data_clean, "lotr_clean.tsv", quote=FALSE,
+			sep="\t", row.names=FALSE)
---
+++ hw07/Step 03-Aggregating Plots.R
@@ -0,0 +1,38 @@
+library(ggplot2)
+library(plyr)
+library(dplyr)
+
+#Import our cleaned lotr data from Step 2
+lotr_data <- read.delim("lotr_clean.tsv")
+
+#Check the structure to make sure factor levels are still intact
+str(lotr_data)
+levels(lotr_data$Race)
+
+#Filter to show total words spoken by the Nine Companions
+fellowship <- c("Gandalf", "Aragorn", "Merry", "Pippin", "Frodo", "Sam", "Gimli", "Legolas", "Boromir")
+characterwords <- lotr_data %>% 
+	filter(Character %in% fellowship) %>% 
+	group_by(Character, Film) %>% 
+	summarize(sum(Words))
+
+#for each Film, write a plot to file of words by fellowship member
+d_ply(characterwords, ~Film, function(p){
+	the_film <- p$Film[1]
+	R <- ggplot(p, aes(Character, `sum(Words)`))+ 
+		geom_col() +
+		theme(axis.text.x=element_text (angle=270)) +
+		labs(y="Total Words Spoken", x= "Nine Companions")+
+		ggtitle(the_film)
+	print(R)
+	the_film <- gsub(" ", "-", the_film)
+	ggsave(paste0("barchart_words-by-companion_", the_film, ".png"))
+})
+
+
+## write the total words by Companion to file
+write.table(ddply(characterwords, ~Character, summarize, total_words=sum(`sum(Words)`)),
+			"total-words-by-Companion.tsv", quote = FALSE,
+			sep = "\t", row.names = FALSE)
+
+
